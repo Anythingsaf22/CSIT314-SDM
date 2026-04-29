94b6ae026d4439602883a4ada8e9b36e7f8ba2cf
yyan021212 <yyan021212@gmail.com>
2026-04-29 20:07:27 +0800

    Update search_fundraising_activity_controller.py

--- control/search_fundraising_activity_controller.py
+++ control/search_fundraising_activity_controller.py
@@ -1,1 +1,10 @@
+from typing import Tuple, Optional
+from entity.fundraising_activity import FundraisingActivity
 
+
+class class search_fundraising_activity_controller:
+    """
+    Control class responsible for searching fundraising activity.
+    """
+    def searchActivities(self, keyword: str) -> List["FundraisingActivity"]:
+        return FundraisingActivity.searchActivities(keyword)
