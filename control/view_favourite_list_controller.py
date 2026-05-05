c0481f9d345537fecd9e1aa34a1db73a709761be
yyan021212 <yyan021212@gmail.com>
2026-05-05 16:15:03 +0800

    Update view_favourite_list_controller.py

--- control/view_favourite_list_controller.py
+++ control/view_favourite_list_controller.py
@@ -1,7 +1,7 @@
 from entity.favourite_list import FavouriteList
 
 
-class view_favourite_controller:
+class view_favourite_list_controller:
 
     def viewFavourites(self, accountId: int):
         return FavouriteList.getFavouritesByAccountId(accountId)
