23600fc5951d7ac70bf5ae635b7078f56f52edd8
yyan021212 <yyan021212@gmail.com>
2026-05-05 16:06:55 +0800

    Update favourite_list_routes.py

--- boundary/favourite_list_routes.py
+++ boundary/favourite_list_routes.py
@@ -1,5 +1,8 @@
 from flask import Blueprint, render_template, request, session, flash, redirect, url_for
-from control.add_favourite_controller import add_favourite_controller
-from control.view_favourite_controller import view_favourite_controller
-from control.search_favourite_controller import search_favourite_controller
+from control.add_favourite_list_controller import add_favourite_list_controller
+from control.view_favourite_list_controller import view_favourite_list_controller
+from control.search_favourite_list_controller import search_favourite_list_controller
 from boundary.access_control import login_required, roles_required, DONOR
+
+favourite_bp = Blueprint("favourite_list", __name__)
+
