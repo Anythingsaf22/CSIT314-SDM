2606ba042da702a1de54be27164be64a4b0f3800
yyan021212 <yyan021212@gmail.com>
2026-05-05 19:40:24 +0800

    Delete control/search_favourite_list_controller.py

--- control/search_favourite_list_controller.py
+++
@@ -1,7 +0,0 @@
-from entity.favourite_list import FavouriteList
-
-
-class search_favourite_list_controller:
-
-    def searchFavourites(self, accountId: int, searchTerm: str):
-        return FavouriteList.searchFavouritesByAccountId(accountId, searchTerm)
