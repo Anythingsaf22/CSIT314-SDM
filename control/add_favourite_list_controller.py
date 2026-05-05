190eb8cc7d2a9b2361a286b7c8a9610502dd3e78
yyan021212 <yyan021212@gmail.com>
2026-05-05 16:15:15 +0800

    Update add_favourite_list_controller.py

--- control/add_favourite_list_controller.py
+++ control/add_favourite_list_controller.py
@@ -2,7 +2,7 @@ from typing import Tuple
 from entity.favourite_list import FavouriteList
 
 
-class add_favourite_controller:
+class add_favourite_list_controller:
 
     def addFavourite(self, accountId: int, activityId: int) -> Tuple[bool, str]:
         return FavouriteList.addFavourite(accountId, activityId)
